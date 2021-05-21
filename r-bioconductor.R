9361477d44355a89072e3bee5b8cb0297c78a731
Ekaterina Aladyeva <46932077+aladyeva-wustl@users.noreply.github.com>
2021-05-21 12:26:18 -0500

    Added R files

---
+++ r-bioconductor.R
@@ -0,0 +1,5 @@
+source("https://bioconductor.org/biocLite.R")
+biocLite(c("BiocInstaller", "rhdf5","pcaMethods"))
+#install.packages('https://cran.r-project.org/src/contrib/BiocManager_1.30.4.tar.gz', repos = NULL)
+#install.packages("BiocManager")
+
---
+++ r-misc.R
@@ -0,0 +1,2 @@
+source("https://bioconductor.org/biocLite.R")
+install.packages(c("tidyverse","ggplot2","Hmisc","plotrix","png", "Matrix", "RJSONIO", "cowplot", "devtools","foreign","latticeExtra","car","rstatix", "ggpubr"),repo=paste0("https://mran.microsoft.com/snapshot/",format(Sys.Date(), format="%Y-%m-%d")))
